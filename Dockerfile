68d32781ffe9133cd4d337f33324d231fc51b8ae
rfernandez <rfernandez@tuenti.com>
2017-06-30 18:01:50 +0200

    Split for backend and frontend

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:6.10.1
-MAINTAINER Roberto Fernandez <rbrlnx@gmail.com>
-RUN mkdir -p /app
-WORKDIR /app
-ADD package.json yarn.lock /app/
-RUN yarn --pure-lockfile
-COPY . /app/
-EXPOSE 4040
-CMD [ "yarn", "start" ]
--- server/routes/index.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import agentRoutes from './agent.route';
-
-const router = express.Router(); // eslint-disable-line new-cap
-
-router.get('/health-check', (req, res) => res.send('OK'));
-router.use('/agent', agentRoutes);
-
-export default router;
